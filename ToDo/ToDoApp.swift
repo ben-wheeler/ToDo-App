8d26f96ebc24590cf657325f7ae88a1acbc06b47
Ben <92675290+ben-wheeler@users.noreply.github.com>
2021-11-23 10:43:34 +1030

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Ben Wheeler on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
