6f57e3b19dd21cfb6db9e1901c33bcb9b3c27243
Ben <92675290+ben-wheeler@users.noreply.github.com>
2021-12-13 15:27:31 +1030

    initialising empty app

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Ben Wheeler on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
