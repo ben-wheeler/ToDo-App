7d397b5e838f237799e03604f44a2a89e2ec6396
Ben <92675290+ben-wheeler@users.noreply.github.com>
2021-12-13 15:45:26 +1030

    current Progress, adding empty toDos

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var toDos = [String]()
+    @State private var newTODO = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            NavigationView {
+                List{
+                    Section {
+                        TextField("New To-Do", text: $newTODO)
+                    }
+                    Section {
+                        ForEach(toDos, id: \.self) { task in
+                            Text(task)
+                        }
+                    }
+                }
+                .navigationTitle("ToDos")
+                .onSubmit {
+                    let answer = newTODO.trimmingCharacters(in: .whitespacesAndNewlines)
+                    
+                    guard answer.count > 0 else { return }
+                                        
+                    toDos.insert(answer, at: 0)
+                    newTODO = ""
+                }
+            }
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
