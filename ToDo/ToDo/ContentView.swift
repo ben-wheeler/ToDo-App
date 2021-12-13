a85e5207f26ff98c91c92a5ecb07ff600137478f
Ben <92675290+ben-wheeler@users.noreply.github.com>
2021-12-13 23:47:46 +1030

    added support for persistant data

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -7,19 +7,34 @@
 
 import SwiftUI
 
-struct TaskItem {
+struct TaskItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
-    let due: String
+    let due: Date
+    let hasTime: Bool
 }
 
 class ToDos: ObservableObject {
-    @Published var items = [TaskItem]()
+    @Published var items = [TaskItem](){
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([TaskItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
 }
 
 struct ContentView: View {
     @StateObject var todos = ToDos()
-    
-    @State private var newTODO = ""
     
     @State private var showingSheet = false
     
@@ -31,100 +46,48 @@ struct ContentView: View {
                         Image(systemName: "circle")
                             .foregroundColor(Color(.systemMint))
                         Text(task.name)
+                            .font(.headline)
+                        Spacer()
                         Image(systemName: "calendar")
-                        Text(task.due)
+                        if(task.hasTime){
+                            Text(task.due.formatted())
+                        }
+                        else{
+                            Text(task.due, format: .dateTime.day().month().year())
+                        }
                     }
+                    .foregroundColor(Date.now > task.due ? .red : .primary)
                 }
                 .onDelete(perform: removeItems)
             }
             .navigationTitle("To-Do List")
-            .toolbar {
-                HStack{
-                EditButton()
-                    Spacer()
-                Button {
-                    let t = TaskItem(name: "Test", due: "23/07/2021")
-                    todos.items.append(t)
-//                    showingSheet.toggle()
-                } label: {
-                    Image(systemName: "plus.circle.fill")
-                }
-            }
-            .sheet(isPresented: $showingSheet) {
-//                NewSheet(currentToDos: todos)
-            }
-            }
+                        .toolbar {
+                            ToolbarItemGroup(placement: .cancellationAction){
+                                EditButton()
+                            }
+                            ToolbarItemGroup(placement: .confirmationAction){
+                                Button {
+                                    showingSheet.toggle()
+                                } label: {
+                                    Image(systemName: "plus.circle.fill")
+                                }
+                            }
+                        }
             
+            .sheet(isPresented: $showingSheet) {
+                NewView(todos: todos)
+            }
         }
+        
     }
+    
     func removeItems(at offsets: IndexSet) {
         todos.items.remove(atOffsets: offsets)
     }
 }
 
-struct NewSheet: View {
-    @Environment(\.dismiss) var dismiss
-    
-    @State private var currentToDos: [String]
-    @State private var newTODO = ""
-    
-    @State private var errorMessage = ""
-    @State private var errorTitle = ""
-    @State private var showError = false
-    
-    var body: some View {
-        NavigationView {
-            List{
-                Section {
-                    TextField("New To-Do Name", text: $newTODO)
-                }
-                
-                .navigationTitle("New To-Do")
-                .toolbar {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(systemName: "xmark.circle.fill")
-                            .foregroundColor(Color(.gray))
-                    }
-                }
-                .onSubmit (addTODO)
-                .alert(errorTitle, isPresented: $showError) {
-                    Button("OK", role: .cancel) { }
-                } message: {
-                    Text(errorMessage)
-                }
-                
-            }
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
-        
-        
-        func addTODO() {
-            let task = newTODO.trimmingCharacters(in: .whitespacesAndNewlines)
-            
-            guard task.count > 0 else { return }
-            guard isNew(word: task) else {
-                errorTitle = "Task already exists"
-                errorMessage = "'\(task)' already exists within To-Do List, sorry! Please enter a task with a different name"
-                showError = true
-                
-                return
-            }
-            
-            withAnimation {
-                currentToDos.insert(task, at: 0)
-            }
-            newTODO = ""
-        }
-        
-        func isNew(word: String) -> Bool {
-            !currentToDos.contains(word)
-        }
-    }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
+}
