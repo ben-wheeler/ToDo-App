30fa9152876d8e7eb10c5b6e95f42745a47902e2
Ben <92675290+ben-wheeler@users.noreply.github.com>
2021-12-13 16:30:24 +1030

    added error checking for duplicate task names

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -11,30 +11,54 @@ struct ContentView: View {
     @State private var toDos = [String]()
     @State private var newTODO = ""
     
+    @State private var errorMessage = ""
+    @State private var errorTitle = ""
+    @State private var showError = false
+    
     var body: some View {
-            NavigationView {
-                List{
-                    Section {
-                        TextField("New To-Do", text: $newTODO)
-                    }
-                    Section {
-                        ForEach(toDos, id: \.self) { task in
-                            Text(task)
-                        }
-                    }
+        NavigationView {
+            List{
+                Section {
+                    TextField("New To-Do", text: $newTODO)
                 }
-                .navigationTitle("ToDos")
-                .onSubmit {
-                    let answer = newTODO.trimmingCharacters(in: .whitespacesAndNewlines)
-                    
-                    guard answer.count > 0 else { return }
-                                        
-                    toDos.insert(answer, at: 0)
-                    newTODO = ""
+                Section {
+                    ForEach(toDos, id: \.self) { task in
+                        Text(task)
+                    }
                 }
             }
+            .navigationTitle("ToDos")
+            .onSubmit (addTODO)
+            .alert(errorTitle, isPresented: $showError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
+            
         }
     }
+    
+    
+    func addTODO() {
+        let task = newTODO.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard task.count > 0 else { return }
+        guard isNew(word: task) else {
+            errorTitle = "Task already exists"
+            errorMessage = "'\(task)' already exists within To-Do List, sorry! Please enter a task with a different name"
+            showError = true
+            return
+        }
+        
+        toDos.insert(task, at: 0)
+        newTODO = ""
+    }
+    
+    func isNew(word: String) -> Bool {
+        !toDos.contains(word)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
