621fae13f2b5a629d94331d904fd5f16f2cd3c3f
Ben <92675290+ben-wheeler@users.noreply.github.com>
2021-12-13 23:47:37 +1030

    created view for new todos

---
+++ ToDo/ToDo/NewView.swift
@@ -0,0 +1,62 @@
+//
+//  NewView.swift
+//  ToDo
+//
+//  Created by Ben Wheeler on 13/12/21.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    
+    @ObservedObject var todos: ToDos
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var due = Date.now
+    @State private var hasTime = false
+    
+    @State private var errorMessage = ""
+    @State private var errorTitle = ""
+    @State private var showError = false
+    
+    var body: some View {
+        NavigationView {
+            Form{
+                Section {
+                    TextField("Name", text: $name)
+                    DatePicker("Due Date", selection: $due, in: Date.now..., displayedComponents: .date)
+                    Toggle("Time", isOn: $hasTime)
+                    if(hasTime){
+                        DatePicker("Due Date", selection: $due, in: Date.now..., displayedComponents: .hourAndMinute)
+                    }
+                }
+            }
+            .navigationTitle("Create a new To-Do")
+            .toolbar {
+                ToolbarItemGroup(placement: .cancellationAction){
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(Color(.gray))
+                    }
+                }
+                ToolbarItemGroup(placement: .confirmationAction){
+                    Button("Save") {
+                        let item = TaskItem(name: name, due: due, hasTime: hasTime)
+                        todos.items.append(item)
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct NewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewView(todos: ToDos())
+    }
+}
